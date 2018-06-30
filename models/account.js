6ab415e9a304d02390504817a9c3cb7cae52fc1a
Bryan Ngo <bngo@ucdavis.edu>
2018-06-30 15:24:39 -0700

    account creation tracks permissions

--- models/account.js
+++ models/account.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
+// http://mongoosejs.com/docs/schematypes.html
 const Account = new Schema({
     username: String,
-    password: String
+    password: String,
+    permission: Number
+    // -- permissions --
+    // 0 = normie
+    // 1 = all-powerful admin (Bryan)
 });
 
 Account.plugin(passportLocalMongoose);
